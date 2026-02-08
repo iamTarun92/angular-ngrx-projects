2a4dbef1f0debd3aba9d5bf7d6db692d792f5a0a
Tarun <iamTarunPandey@outlook.com>
2026-02-08 20:52:35 +0530

    html part done

--- project_01/src/app/app.component.ts
+++ project_01/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BucketComponent } from './components/bucket/bucket.component';
+import { GroceryComponent } from './components/grocery/grocery.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BucketComponent,GroceryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
